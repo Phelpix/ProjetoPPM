26e6d5f8d22a160bfcdc73d8b830e327f394824c
mmelo123 <mjrmo2@iscte-iul.pt>
2020-11-18 14:39:03 +0000

    commits fodidos palmadas no ass manos tao jealous pq eu tou blessed

--- src/ExpensesTracker/Menu.scala
+++ src/ExpensesTracker/Menu.scala
@@ -1,13 +1,15 @@
 package ExpensesTracker
 
+import java.text.SimpleDateFormat
+import java.io.IOException
 import ExpensesTracker.MenuUtils._
 
 import scala.io.StdIn.readLine
-
+import java.util.Calendar
 
 import scala.::
 
-case class UserApp(name: String, balance:Double, depositList: List[(Double, String)], expenseList: List[(Double, String)])
+case class UserApp(name: String, balance:Double, depositList: List[(Double, String, Any)], expenseList: List[(Double, String, Any)])
 
 object Menu extends App{
 
@@ -24,34 +26,10 @@ object Menu extends App{
 
     userInput match {
       case "1" => {
-        println("\n\n\n\n **** QUANTO VAI DEPOSITAR? ****\n")
-        val newDepositedValue:Double = getUserInput().toDouble
-        print("\n DESCRIÇAO DO SEU DEPOSITO:")
-        val DepositDescription: String = getUserInput()
-        val newBalance: Double = user.balance + newDepositedValue
-        val newDepositList: List[(Double, String)] = List((newDepositedValue,DepositDescription)) ::: user.depositList
-        val newUserApp = {
-          user.copy(name = user.name, balance = newBalance, depositList = newDepositList, expenseList = user.expenseList)
-        }
-        println("\n\n\n\n DEPOSITO REALIZADO COM SUCESSO \n\n TEM AGORA "+ newBalance+" NA SUA CONTA")
-        println(newDepositList)
-        Thread.sleep(3000)
-        mainLoop(newUserApp)
+        income(user)
       }
       case "2" => {
-        println("\n\n\n\n **** QUAL FOI O VALOR DA SUA COMPRA? ****\n")
-        val newExpenseValue: Double = getUserInput().toDouble
-        print("\n DESCRIÇAO DA COMPRA:")
-        val ExpenseDescription: String = getUserInput()
-        val newBalance: Double= user.balance - newExpenseValue
-        val newExpenseList: List[(Double, String)] = ((List((newExpenseValue, ExpenseDescription)) ::: user.expenseList).reverse).reverse
-        val newUserApp = {
-          user.copy(name = user.name, balance = newBalance, depositList = user.depositList, expenseList = newExpenseList)
-        }
-        println("\n\n\n\n COMPRA REALIZADA COM SUCESSO \n\n TEM AGORA "+ newBalance+" NA SUA CONTA")
-        println(newExpenseList)
-        Thread.sleep(3000)
-        mainLoop(newUserApp)
+        expense(user)
       }
       case "3"=>{
         println("\n\n\n\n **** O VALOR DA SUA BALANCA E " + user.balance +" ****\n")
@@ -62,6 +40,9 @@ object Menu extends App{
         filtros(user)
         Thread.sleep(3000)
         mainLoop(user)
+      }
+      case _ =>{
+        mainLoop(x)
       }
     }
 
--- src/ExpensesTracker/MenuUtils.scala
+++ src/ExpensesTracker/MenuUtils.scala
@@ -1,14 +1,19 @@
 package ExpensesTracker
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import ExpensesTracker.Menu.{mainLoop, x}
+
 import scala.io.StdIn.readLine
 
 object MenuUtils {
   def showPrompt(): Unit ={
-    print("\n escolha o número:")
-    println("\n 1-deposite")
-    println("\n 2-compra")
-    println("\n 3-balanço")
-    println("\n 4-filtro")
+    println("\n escolha o número:")
+    println("\n 1-depositar")
+    println(" 2-compra")
+    println(" 3-balanço")
+    println(" 4-filtro")
   }
 
   def getUserInput(): String = readLine.trim.toUpperCase
@@ -24,19 +29,69 @@ object MenuUtils {
       case "2" =>{
         showElements(x.depositList)
       }
-
     }
   }
 
   def showFilters(): Unit ={
-    print("\n escolha o número:")
-    println("\n 1-compras")
-    println("\n 2-depositos")
-    println("\n 3-comida")
-    println("\n 4-carro")
+    println("\n escolha o número:")
+    println(" 1-compras")
+    println(" 2-depositos")
+    println(" 3-comida")
+    println(" 4-carro")
   }
 
-  def showElements(list :List[(Double, String)]): Unit ={
+  def income(user:UserApp) {
+    try {
+      println("\n\n\n\n **** QUANTO VAI DEPOSITAR? ****\n")
+      val newDepositedValue: Double = getUserInput().toDouble
+      print("\n DESCRIÇAO DO SEU DEPOSITO:")
+      val DepositDescription: String = getUserInput()
+      val newBalance: Double = user.balance + newDepositedValue
+      val format = new SimpleDateFormat("d-M-y H:m")
+      val newDepositList: List[(Double, String, Any)] = List((newDepositedValue, DepositDescription, format.format(Calendar.getInstance().getTime()))) ::: user.depositList
+      val newUserApp = {
+        user.copy(name = user.name, balance = newBalance, depositList = newDepositList, expenseList = user.expenseList)
+      }
+      println("\n\n\n\n DEPOSITO REALIZADO COM SUCESSO \n\n TEM AGORA " + newBalance + " NA SUA CONTA")
+      Thread.sleep(3000)
+      mainLoop(newUserApp)
+    }catch{
+      case ex: NumberFormatException => {
+        println("Insira um valor válido")
+        income(user)
+
+      }
+    }
+  }
+
+  def expense(user:UserApp) {
+    println("\n\n\n\n **** QUAL FOI O VALOR DA SUA COMPRA? ****\n")
+
+    try {
+      val newExpenseValue: Double = getUserInput().toDouble
+      print("\n DESCRIÇAO DA COMPRA:")
+      val ExpenseDescription: String = getUserInput()
+      val newBalance: Double = user.balance - newExpenseValue
+      val format = new SimpleDateFormat("d-M-y H:m")
+      val newExpenseList: List[(Double, String, Any)] = ((List((newExpenseValue, ExpenseDescription, format.format(Calendar.getInstance().getTime()))) ::: user.expenseList).reverse).reverse
+      val newUserApp = {
+        user.copy(name = user.name, balance = newBalance, depositList = user.depositList, expenseList = newExpenseList)
+      }
+      println("\n\n\n\n COMPRA REALIZADA COM SUCESSO \n\n TEM AGORA " + newBalance + " NA SUA CONTA")
+      Thread.sleep(3000)
+      mainLoop(newUserApp)
+    } catch {
+      case ex: NumberFormatException => {
+        println("Insira um valor válido")
+        expense(user)
+
+      }
+
+
+    }
+  }
+
+  def showElements(list :List[(Double, String, Any)]): Unit ={
     list match{
       case x ::t => {println(x)
         showElements(t)
